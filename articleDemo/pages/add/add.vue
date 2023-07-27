e5e11d7df24b0765584ce8e7e7f233bd75f27ded
xiaoxiaoyan1223 <97817462+xiaoxiaoyan1223@users.noreply.github.com>
2023-07-27 16:38:33 +0800

    Update add.vue

--- articleDemo/pages/add/add.vue
+++ articleDemo/pages/add/add.vue
@@ -2,14 +2,24 @@
 	<view class="add" >
 		<form @submit="onSubmit">
 			<view class="item">
-				<input v-model="formValue.title" type="text" name="title"><input/>
+				<input v-model="formValue.title" type="text" name="title" placeholder="请输入标题"><input/>
 			<view/>
 			<view class="item">
-				<input v-model="formValue.author" type="text" name="author"><input/>
+				<input v-model="formValue.author" type="text" name="author" placeholder="请输入作者"><input/>
 			<view/>
 			<view class="item">
-				<textarea v-model="formValue.content" name="content"></textarea> 
+				<textarea v-model="formValue.content" name="content" placeholder="请输入详细内容"></textarea> 
 			<view/>
+			<view class="item">
+				<uni-file-picker 
+					v-model="imageValue" 
+					fileMediatype="image" 
+					mode="grid" 
+					@select="select" 
+					@success="uploadSuccess" 
+					@fail="fail" 
+				/>
+			</view>
 			<view class="item">
 				<button form-type="reset">重置</button>
 				<button form-type="submit" type="primary" :disabled="inDisable()">提交</button>
@@ -26,10 +36,15 @@
 					title:"",
 					author:"",
 					content:""
-				}
+				},
+				picurls:[]
 			}
 		},
 		methods: {
+			//图片上传成功
+			uploadSuccess(e){
+				this.picurls=e.tempFilePaths
+			}
 			//判断提交按钮是否禁用
 			inDisable(obj){
 				for(let key in obj){
@@ -44,7 +59,8 @@
 			  uniCloud.callFunction({
 			  	name:"art_add_row",
 				data:{
-					detail
+					detail,
+					picurls
 				}
 			  }).then(res=>{
 				  uni.showToast({
